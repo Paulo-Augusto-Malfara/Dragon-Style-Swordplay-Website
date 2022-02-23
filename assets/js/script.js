ee081337acdbb81fa2bd1208357a3e29301039ac
Paulo Augusto <paulo_papito@hotmail.com>
2022-02-23 03:04:58 -0300

    navigation menu responsive layout 1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 /* RESPONSIVE NAVIGATION MENU */
 
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("navbar")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const toggleButtonActive = document.getElementsByClassName("button-toggle")[0];
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
+  toggleButtonActive.classList.toggle("active");
 });
