af0beab76bb39665b50eabc67823562b337b5a67
Paulo Augusto <paulo_papito@hotmail.com>
2022-02-25 00:39:24 -0300

    main min-height ajuste para 1080px

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,39 +12,20 @@
   });
 })();
 
-/* FUNÇÃO QUE FUNCIONA SOMENTE NO 1º BOTÃO */
-
 (function dinamicModalidades() {
-  //const toggleButton = document.querySelector(".modalidades-button");
-  // toggleButton.addEventListener("click", () => {
-  //   modalidadesConteudo.classList.toggle("active-modalidades");
-  //   modalidadesH2.classList.toggle("active-modalidades");
-  // });
   document.addEventListener("click", (e) => {
     const el = e.target;
-    const modalidadesConteudo = document.getElementsByClassName("modalidades-conteudo")[document.activeElement.classList.value - 1];
-    const modalidadesH2 = document.getElementsByClassName("h2-bottom-border")[document.activeElement.classList.value - 1];
+    const modalidadesConteudo = document.getElementsByClassName(
+      "modalidades-conteudo"
+    )[document.activeElement.classList.value - 1];
+    const modalidadesH2 =
+      document.getElementsByClassName("h2-bottom-border")[
+        document.activeElement.classList.value - 1
+      ];
     if (el.classList.contains("modalidades-button")) {
       modalidadesConteudo.classList.toggle("active-modalidades");
       modalidadesH2.classList.toggle("active-modalidades");
     }
-    console.log (document.activeElement.classList.value);
+    console.log(document.activeElement.classList.value);
   });
 })();
-
-// (function dinamicModalidadesTeste1() {
-//   document.addEventListener("DOMContentLoaded", () => {
-//     let myBtns = document.querySelectorAll(".modalidades-button");
-//     let modalidadesConteudo = document.querySelectorAll(
-//       ".modalidades-conteudo"
-//     );
-//     myBtns.forEach(function (btn) {
-//       btn.addEventListener("click", () => {
-//         modalidadesConteudo.forEach((b) =>
-//           b.classList.remove("active-modalidades")
-//         );
-//         btn.classList.add("active-modalidades");
-//       });
-//     });
-//   });
-// })();
