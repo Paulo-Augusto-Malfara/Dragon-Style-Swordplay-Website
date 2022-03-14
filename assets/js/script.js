fe71341824316cb3a5890c21c45c6023014008aa
Paulo Augusto <paulo_papito@hotmail.com>
2022-03-14 01:54:55 -0300

    testes pagina individual de classe

--- assets/js/script.js
+++ assets/js/script.js
@@ -132,6 +132,26 @@ function carregaResultado(response) {
   resultado.innerHTML = response;
 }
 
+/* SLIDES */
+
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
 /* RESPONSIVE NAVIGATION MENU */
 
 (function responsiveNavigationMenu() {
